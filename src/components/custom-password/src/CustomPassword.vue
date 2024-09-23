17d23726562d57de5a56f1262b90b76b1b1411b0
Raze <ze.wang@diact.com>
2024-09-23 16:36:00 +0800

    first commit

---
+++ src/components/custom-password/src/CustomPassword.vue
@@ -0,0 +1,29 @@
+<script lang="ts" setup>
+import { useAttrs, computed } from 'vue'
+import icon_visible_outlined from '@/assets/svg/icon_visible_outlined.svg'
+import icon_invisible_outlined from '@/assets/svg/icon_invisible_outlined.svg'
+import { hIcon } from '@/components/icon-custom'
+const attrs = useAttrs()
+const props = defineProps(['modelValue'])
+const emits = defineEmits(['update:modelValue'])
+const value = computed({
+  get() {
+    return props.modelValue
+  },
+  set(value) {
+    emits('update:modelValue', value)
+  }
+})
+
+const iconView = hIcon(icon_visible_outlined)
+const iconHide = hIcon(icon_invisible_outlined)
+</script>
+
+<template>
+  <el-input
+    :icon-view-custom="iconView"
+    :icon-hide-custom="iconHide"
+    v-bind="attrs"
+    v-model="value"
+  />
+</template>
